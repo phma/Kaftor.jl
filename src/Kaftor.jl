d95dd60f46caa5453132fdddade4e2996b38c425
Pierre Abbat <phma@bezitopo.org>
2026-07-20 06:40:37 +0000

    Unrotate a 2-byte number by a key in parts

--- src/Kaftor.jl
+++ src/Kaftor.jl
@@ -1,5 +1,5 @@
 module Kaftor
-export rot4p
+export rot4p,unrot4p
 
 """
     rot4p(n::UInt16,key::UInt8)
@@ -19,6 +19,26 @@ function rot4p(n::UInt16,key::UInt8)
   p5=(((n&0x1f)*0x00021)>>(5-b5))&0x1f
   p11=(((((n&0xffe0)>>5)*0x00801)>>(11-b11))&0x7ff)<<5
   UInt16(p11|p5)
+end
+
+"""
+    unrot4p(n::UInt16,key::UInt8)
+
+Rotates `n` in two parts of 5 and 11 bits, then exclusive-ors `n` with `key`,
+then rotates `n` in two more parts of 9 and 7 bits, the number of bits rotated
+depending on `key`. Undoes `rot4p`.
+"""
+function unrot4p(n::UInt16,key::UInt8)
+  b5 =(key+1024)%5
+  b7 =(key+1024)%7
+  b9 =(key+1024)%9
+  b11=(key+1024)%11
+  p5=(((n&0x1f)*0x00021)>>b5)&0x1f
+  p11=(((((n&0xffe0)>>5)*0x00801)>>b11)&0x7ff)<<5
+  n=(p11|p5)⊻key
+  p9=(((n&0x1ff)*0x00201)>>b9)&0x1ff
+  p7=(((((n&0xfe00)>>9)*0x00081)>>b7)&0x7f)<<9
+  UInt16(p7|p9)
 end
 
 end # module Kaftor
