35323c629f681510bb045ffe2ad5619cce880009
Pierre Abbat <phma@bezitopo.org>
2026-07-21 07:42:35 +0000

    Test rot4p

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,14 @@
 using Kaftor,Test
+
+function testRot4p(n::Int)
+  a=UInt8(n>>16&255)
+  b=UInt8(n>>8&255)
+  key=UInt8(n&255)
+  c,d=rot4p(a,b,key)
+  e,f=unrot4p(c,d,key)
+  a==e && b==f
+end
+
+for i in 0:465:1048575
+  @test testRot4p(i)
+end
