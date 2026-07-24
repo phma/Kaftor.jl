caf4d693c26820da0f8be648c458db873897cf02
Pierre Abbat <phma@bezitopo.org>
2026-07-24 07:40:18 +0000

    Add Jumble, modified from the randomness extractor

---
+++ src/Jumble.jl
@@ -0,0 +1,45 @@
+module Jumble
+# Modified from code I wrote for Real Random
+include("Mix3.jl")
+using Base.Threads,Primes,Mods
+using .Mix3
+export jumble!
+
+function jumbleWorker!(data::Vector{<:Integer},p,q,r,n,t,imod,jmod,thue)
+  len=length(data)
+  while n<=(p-3)÷2
+    i=value(imod)-1
+    j=value(jmod)-1
+    if i<=len && j<=len
+      (data[i],data[j])=(data[i]⊻data[j]&thue,data[j]⊻data[i]&~thue)
+    end
+    imod*=r
+    jmod*=q
+    n+=t
+  end
+end
+
+"""
+  jumble!(data::Vector{<:Integer})
+
+Make bytes in `data` affect each other if their positions are multiplicative
+inverses mod the next prime. The function is an involution.
+"""
+function jumble!(data::Vector{<:Integer})
+  len=length(data)
+  p=nextprime(len+3) # skip -1, 0, and 1
+  r=Mod{p}(findMaxOrder(p))
+  q=inv(Mod{p}(r))
+  thue=0x9669699669969669&(typemax(eltype(data))⊻typemin(eltype(data)))
+  t=nthreads()
+  tasks=Task[]
+  for n in 1:t
+    push!(tasks,@spawn jumbleWorker!(data,p,q^t,r^t,$n,t,$r^n,$q^n,thue))
+  end
+  for i in 1:t
+    wait(tasks[i])
+  end
+  return nothing
+end
+
+end
--- src/Kaftor.jl
+++ src/Kaftor.jl
@@ -1,6 +1,7 @@
 module Kaftor
 include("Rotate4Parts.jl")
 include("Mix3.jl")
+include("Jumble.jl")
 using OffsetArrays
 using .Rotate4Parts,.Mix3
 export rot4p,unrot4p
