b619cad101cab0bd42cad82dac866299e3494708
Pierre Abbat <phma@bezitopo.org>
2026-07-31 20:49:04 +0000

    Test mix3PartsSeq!

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using Kaftor,Test
-using Kaftor.Jumble
+using Kaftor.Jumble,Kaftor.Mix3
 
 function testRot4p(n::Int)
   a=UInt8(n>>16&255)
@@ -21,4 +21,13 @@ function testJumble(buf::Vector{UInt8})
   orig==buf
 end
 
+function testMix3(buf::Vector{UInt8})
+  orig=copy(buf)
+  rprime=findMaxOrder(length(buf)÷3)
+  mix3PartsSeq!(buf,rprime)
+  mix3PartsSeq!(buf,rprime)
+  orig==buf
+end
+
 @test testJumble(collect(0x00:0xa2))
+@test testMix3(collect(0x00:0xa2))
