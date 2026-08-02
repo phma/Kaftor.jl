91b91dbef43cf5038cef8b4bf832d9b69aaf076e
Pierre Abbat <phma@bezitopo.org>
2026-08-02 07:15:24 +0000

    Flip half the bits on average each round

--- src/ShufflePairs.jl
+++ src/ShufflePairs.jl
@@ -16,7 +16,7 @@ function rot4p(n::UInt16,key::UInt8)
   b11=(key+1024)%11
   p9=(((n&0x1ff)*0x00201)>>(9-b9))&0x1ff
   p7=(((((n&0xfe00)>>9)*0x00081)>>(7-b7))&0x7f)<<9
-  n=(p7|p9)⊻key
+  n=(p7|p9)⊻(key|0x6900)
   p5=(((n&0x1f)*0x00021)>>(5-b5))&0x1f
   p11=(((((n&0xffe0)>>5)*0x00801)>>(11-b11))&0x7ff)<<5
   UInt16(p11|p5)
@@ -36,7 +36,7 @@ function unrot4p(n::UInt16,key::UInt8)
   b11=(key+1024)%11
   p5=(((n&0x1f)*0x00021)>>b5)&0x1f
   p11=(((((n&0xffe0)>>5)*0x00801)>>b11)&0x7ff)<<5
-  n=(p11|p5)⊻key
+  n=(p11|p5)⊻(key|0x6900)
   p9=(((n&0x1ff)*0x00201)>>b9)&0x1ff
   p7=(((((n&0xfe00)>>9)*0x00081)>>b7)&0x7f)<<9
   UInt16(p7|p9)
