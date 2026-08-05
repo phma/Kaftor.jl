b68ae19a8181800073c4d3cc570ca92ba6b9b37e
Pierre Abbat <phma@bezitopo.org>
2026-08-05 06:41:14 +0000

    Add a profiling function
    The graph doesn't make much sense with multiple threads.

--- bench/src/bench.jl
+++ bench/src/bench.jl
@@ -1,12 +1,18 @@
 module bench
-using Kaftor,BenchmarkTools
-export kaftorTime
+using Kaftor,BenchmarkTools,ProfileView
+export kaftorTime,kaftorProf
 
 function kaftorTime(textLen::Integer,keyLen::Integer) # in nanoseconds
   text=fill(0x69,textLen)
   key=fill(0x96,keyLen)
   trial=@benchmark kaftorEncrypt!($text,$key)
   median(trial).time
+end
+
+function kaftorProf(textLen::Integer,keyLen::Integer)
+  text=fill(0x69,textLen)
+  key=fill(0x96,keyLen)
+  @profview kaftorEncrypt!(text,key)
 end
 
 end
