f6ddf4f19cd5e753cfa5f89c992445dd2076ef19
Pierre Abbat <phma@bezitopo.org>
2026-08-11 02:04:58 -0400

    Copy big3Power and big5Power from WringTwistreeCryptanalysis

--- stats/src/stats.jl
+++ stats/src/stats.jl
@@ -1,9 +1,13 @@
 module stats
 using Kaftor
-export aoeu
+export big3Power,big5Power
 
-function aoeu()
-  println("aoeu")
+function big3Power(n::Integer)
+  big(3)^(n*53÷84)
+end
+
+function big5Power(n::Integer)
+  big(5)^(n*146÷339)
 end
 
 end
