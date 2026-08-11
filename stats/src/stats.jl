d879e5d35c579ec79b25b3dfde03f9b3e8df5390
Pierre Abbat <phma@bezitopo.org>
2026-08-11 02:46:08 -0400

    Convert a bignum to a byte vector and back

--- stats/src/stats.jl
+++ stats/src/stats.jl
@@ -1,6 +1,6 @@
 module stats
 using Kaftor
-export big3Power,big5Power
+export big3Power,big5Power,messageArray,messageBignum
 
 function big3Power(n::Integer)
   big(3)^(n*53÷84)
@@ -8,6 +8,23 @@ end
 
 function big5Power(n::Integer)
   big(5)^(n*146÷339)
+end
+
+function messageArray(pt::Integer,clutchMsgLen::Integer) # also used for keys
+  ret=Vector{UInt8}(undef,clutchMsgLen)
+  for i in 1:clutchMsgLen
+    ret[i]=UInt8(pt&0xff)
+    pt>>=8
+  end
+  ret
+end
+
+function messageBignum(text::Vector{UInt8})
+  ret=big(0)
+  for i in reverse(eachindex(text))
+    ret=ret*256+text[i]
+  end
+  ret
 end
 
 end
